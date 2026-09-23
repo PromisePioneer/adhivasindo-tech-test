4bf7c1b9475b46aca0f376fc5553d345dd034fc5
Firman <fifirman000@gmail.com>
2026-09-23 18:31:27 +0700

    feat: add database seeder for User

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,7 +20,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@local.id',
+            'password' => Hash::make('password'),
         ]);
     }
 }
